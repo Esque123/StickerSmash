b82feaa9aff587f1a84998d105cc22090e5d879f
Esque123 <mostert.k@gmail.com>
2025-02-17 10:56:12 +0200

    Chapter2 Complete

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Sticker Smash",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={30}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              size={30}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
