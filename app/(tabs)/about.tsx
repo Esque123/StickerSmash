14eaa7a0fc79c38818ef4b3bd4aee642ee56874e
Esque123 <mostert.k@gmail.com>
2025-02-17 21:47:17 +0200

    Chapter4 Done

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>The About Page!!</Text>
+      <Text style={styles.text}>This is the about screen</Text>
     </View>
   );
 }
